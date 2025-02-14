9ddad649066687602f2d38c1bf717503038e07b7
gurubazawada <110350867+gurubazawada@users.noreply.github.com>
2025-02-13 20:32:50 -0500

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+    const ChatBoard = await ethers.getContractFactory("ChatBoard");
+    const chatBoard = await ChatBoard.deploy();
+  
+    await chatBoard.waitForDeployment();
+    console.log("ChatBoard deployed to:", await chatBoard.getAddress());
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+  
